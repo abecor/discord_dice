8877bd4a5268bdefd88a8d6670d7593cd6c8b048
abecor <mail@philippmekus.de>
2025-02-25 18:58:30 +0100

    Fixed multiple d20 rolls, repeating roll input

--- roll_dice.py
+++ roll_dice.py
@@ -10,48 +10,53 @@ def is_crit_or_fail(number):
 
 def roll_d20(text):
 
-    result_string = ""
-    result = 0
+    if text.split("d")[0] == "" or  text.split("d")[0] == "1":
 
-    dice_modifier = 0
-    dice_modifier_string = ""
-    critical = ""
-    result_1 = rnd.randint(1, 20)
-    result_2 = rnd.randint(1, 20)
+        result_string = ""
+        result = 0
 
-    if "+" in text:
-        dice_modifier = text.split("+")[1]
+        dice_modifier = 0
+        dice_modifier_string = ""
+        critical = ""
+        result_1 = rnd.randint(1, 20)
+        result_2 = rnd.randint(1, 20)
+
+        if "+" in text:
+            dice_modifier = text.split("+")[1]
+            
+            try:
+                dice_modifier = int(dice_modifier)
+                dice_modifier_string = f" + {dice_modifier}"
+            except ValueError as e:  
+                return f"Invalid modifier. ({e})"
         
-        try:
-            dice_modifier = int(dice_modifier)
-            dice_modifier_string = f" + {dice_modifier}"
-        except ValueError as e:  
-            return f"Invalid modifier. ({e})"
-    
-    dice_modifier_string = dice_modifier_string.removesuffix(" + 0")
+        dice_modifier_string = dice_modifier_string.removesuffix(" + 0")
 
-    # Advantage
-    if "d20a" in text:
-        result = max(result_1, result_2)
-        critical = is_crit_or_fail(result)
-        result = result + dice_modifier
-        result_string += f"{critical}Result: {result} ({result_1} | {result_2}{dice_modifier_string})"
-    
-    # Disadvante
-    elif "d20d" in text:
-        result = min(result_1, result_2)
-        critical = is_crit_or_fail(result)
-        result = result + dice_modifier
-        result_string += f"{critical}Result: {result} ({result_1} | {result_2}{dice_modifier_string})"
+        # Advantage
+        if "d20a" in text:
+            result = max(result_1, result_2)
+            critical = is_crit_or_fail(result)
+            result = result + dice_modifier
+            result_string += f"{critical}Result: {result} ({result_1} | {result_2}{dice_modifier_string})"
+        
+        # Disadvantage
+        elif "d20d" in text:
+            result = min(result_1, result_2)
+            critical = is_crit_or_fail(result)
+            result = result + dice_modifier
+            result_string += f"{critical}Result: {result} ({result_1} | {result_2}{dice_modifier_string})"
 
-    # Straight roll
+        # Straight roll
+        else:
+            result = result_1
+            critical = is_crit_or_fail(result)
+            result = result + dice_modifier
+            result_string += f"{critical}Result: {result} ({result_1}{dice_modifier_string})"
+
+        return result_string
+    
     else:
-        result = result_1
-        critical = is_crit_or_fail(result)
-        result = result + dice_modifier
-        result_string += f"{critical}Result: {result} ({result_1}{dice_modifier_string})"
-    
-    return result_string
+        return roll_ndn(text)
 
 def roll_ndn(text):
     
@@ -91,11 +96,13 @@ def roll_ndn(text):
     print(f"Type: {dice_type}")
     print(f"Modifier: {dice_modifier}")
 
-    if dice_amount <= 10:
+    if  1 <= dice_amount <= 50:
         for i in range(dice_amount):
             roll = rnd.randint(1, dice_type)
             result += roll
             result_string += f"{roll} + "
+    else: 
+        return "That's too many dice!"
     
     result += dice_modifier
     
@@ -106,13 +113,12 @@ def roll_ndn(text):
     else:
         return f"Result: {result} (({result_string}) + {dice_modifier})"
 
-    
 def roll(text):
     if "d20" in text:
-        return (roll_d20(text))
+        return f"Rolling {text}\n{(roll_d20(text))}"
     
     elif "d" in text:
-        return (roll_ndn(text))
+        return f"Rolling {text}\n{(roll_ndn(text))}"
     
     elif "help" in text:
         return "/r roll 2d6+3 for damage dice. Can roll dice in this format: <amount>d<type>+<modifier.\n/r roll d20a+4 for D20 with advantage.\n/r roll d20d+4 for D20 with disadvantage."
